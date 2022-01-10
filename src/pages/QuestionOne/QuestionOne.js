e92b8f4cdb416e423283286a7085a600fb8a3b76
EduardoLovo <eduardo.llovo@gmail.com>
2022-01-09 21:24:28 -0300

    update questio one

--- src/pages/QuestionOne/QuestionOne.js
+++ src/pages/QuestionOne/QuestionOne.js
@@ -1,3 +1,9 @@
+
+// Irá retornar uma div com o fundo verde escrito 'Esta é uma tela' e 'Texto personalizado aqui'.
+
+// Will return a div with the background green written 'This is a screen' and 'Custom text here'.
+
+
 import React from 'react'
 import { ComponentOne } from './ComponentOne'
 
