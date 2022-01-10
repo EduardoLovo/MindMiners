70f7fdb0332deaf1c7cc2f427b910f91348fa37b
EduardoLovo <eduardo.llovo@gmail.com>
2022-01-09 21:32:02 -0300

    Add button 'voltar' in question one

--- src/pages/QuestionOne/QuestionOne.js
+++ src/pages/QuestionOne/QuestionOne.js
@@ -5,12 +5,15 @@
 
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { ComponentOne } from './ComponentOne'
 
 export const QuestionOne = () => {
     return (
         <div>
             <ComponentOne color="green" text="Custom text here" />
+
+            <Link to='/'><button>Voltar</button></Link>
         </div>
     )
 }
